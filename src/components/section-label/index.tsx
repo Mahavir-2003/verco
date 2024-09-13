79b2ca3de71b1f9a0e3cf16eace24b6319ec6399
Mahavir-2003 <patelmahavir2003@outlook.com>
2024-09-13 22:39:08 +0530

    Optimize UI components and create Appoinments route

--- src/components/section-label/index.tsx
+++ src/components/section-label/index.tsx
@@ -3,13 +3,14 @@ import React from 'react'
 type SectionProps = {
   label: string
   message: string
+  darkText?: boolean
 }
 
-const Section = ({ label, message }: SectionProps) => {
+const Section = ({ label, message , darkText = false }: SectionProps) => {
   return (
-    <div >
-      <p className="text-lg font-medium text-white">{label}</p>
-      <p className="text-sm font-light text-white/80">{message}</p>
+    <div>
+      <p className={`text-lg font-medium ${darkText ? 'text-black' : 'text-white'}`}>{label}</p>
+      <p className={`text-sm font-light ${darkText ? 'text-black/80' : 'text-white/80'}`}>{message}</p>
     </div>
   )
 }
